3678e607e2aa6af64caa6229e0e6a429f7aae31c
jpsymfony <jpsymfony@free.fr>
2015-12-25 17:46:25 +0100

    installed pagerFanta and implemented common core bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Back\CoreBundle\BackCoreBundle(),
             new Front\CoreBundle\FrontCoreBundle(),
+            new Common\CoreBundle\CommonCoreBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
